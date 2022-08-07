150a6c0ca0e846c615182b181842e9006d2cbe03
kasturi28 <kasturipansare2818@gmail.com>
2022-08-07 12:58:22 +0530

    Added_prime_number_between_0&1000

--- src/practiseproblems/PrimeNumbers.java
+++ src/practiseproblems/PrimeNumbers.java
@@ -1,7 +1,42 @@
 package practiseproblems;
 
 public class PrimeNumbers {
-	
-	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		/*
+		 * Calling check method to print the required output
+		 */
+		check();
+	}
+
+	/*
+	 * Defing int i , num & empty string primenumbers
+	 */
+	private static void check() {
+		int i = 0;
+		int num = 0;
+		String primeNumbers = "";
+		/*
+		 * Taking counter variable checking number's factors if the count is equal to 2
+		 * then the number will be appended to the string
+		 */
+		for (i = 1; i <= 1000; i++) {
+			int counter = 0;
+			for (num = i; num >= 1; num--) {
+				if (i % num == 0) {
+					counter = counter + 1;
+				}
+			}
+			if (counter == 2) {
+				// Appended the Prime number to the String
+				primeNumbers = primeNumbers + i + " ";
+			}
+		}
+		System.out.println("Prime numbers from 1 to 1000 are :");
+		System.out.println(primeNumbers);
+	}
 
 }
